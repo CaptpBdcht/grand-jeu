c88291f9f86106c123846857451db74e711c9cd4
Captp <vincent_mot@hotmail.fr>
2019-02-09 18:59:18 +0100

    Completed OrientedAssociations fil

--- src/business/data/oriented-associations.ts
+++ src/business/data/oriented-associations.ts
@@ -1,32 +1,32 @@
 export const OrientedAssociations = {
   '2': {
     '7': { H: 'Nouvelles', B: 'Femme de mauvaise vie' },
-    '8': { H: '', B: '' },
-    '9': { H: '', B: '' },
-    '10': { H: '', B: '' },
+    '8': { H: 'Relation nouvelle', B: 'Obstacles ' },
+    '9': { H: 'Petite rentrée d’argent ', B: 'Affaire fructueuse ' },
+    '10': { H: 'Changements ', B: 'Attente de nouvelles ' },
     'V': { H: 'Inquiètude', B: 'Visite' },
     'D': { H: 'Bonne amitié', B: 'Travail' },
     'R': { H: 'Consultation', B: 'Projet' },
-    'A': { H: '', B: '' }
+    'A': { H: 'Trahison ', B: 'Complot d’ennemis ' }
   },
   '3': {
     '7': { H: 'Contrariété', B: 'Allégresse' },
-    '8': { H: '', B: '' },
+    '8': { H: 'Union mal assortie ', B: 'Théâtre ' },
     '9': { H: 'Succès moyen', B: 'Imprudence' },
     '10': { H: 'Changement de situation', B: 'Déception' },
-    'V': { H: '', B: '' },
-    'D': { H: '', B: '' },
-    'R': { H: '', B: '' },
-    'A': { H: '', B: '' }
+    'V': { H: 'Bataille ', B: 'Nonchalance ' },
+    'D': { H: 'Duperie ', B: 'Invitation à dîner ' },
+    'R': { H: 'Grand conseil ', B: 'Réussite commerciale ' },
+    'A': { H: 'Bon profit ', B: 'Vie déréglée ' }
   },
   '4': {
     '7': { H: 'Intrigues ténébreuses', B: 'Bataille' },
     '8': { H: 'Ruine', B: 'Tromperie' },
-    '9': { H: '', B: '' },
-    '10': { H: '', B: '' },
-    'V': { H: '', B: '' },
-    'D': { H: '', B: '' },
-    'R': { H: '', B: '' },
-    'A': { H: '', B: '' }
+    '9': { H: 'Heureuse surprise ', B: 'Désagréable surprise ' },
+    '10': { H: 'Procès ', B: 'Grande nouvelle ' },
+    'V': { H: 'Maladie grave ', B: 'Misère ' },
+    'D': { H: 'Pourparlers ', B: 'Inconduite déplorable ' },
+    'R': { H: 'Grand changement ', B: 'Affaires prospères ' },
+    'A': { H: 'Triomphe complet ', B: 'Nouvelle surprenante et désagréable ' }
   }
 };
