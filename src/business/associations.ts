e6408ede118d597ef2f6d83f5a6a118e9624f42f
Captp <vincent_mot@hotmail.fr>
2019-02-09 17:33:09 +0100

    Added empty oriented-associations

--- src/business/associations.ts
+++ src/business/associations.ts
@@ -21,14 +21,16 @@ export class Associations {
     for (const value of values) {
       for (const orientation of orientations) {
         const foundCards = cards.filter(c => c.getValue() === Values[ value ] && c.getOrientation() === Orientations[ orientation ]);
-        maybeAddOrientedAssociation(foundCards, value, orientation);
+        maybeAddOrientedAssociation(foundCards);
         maybeAddThreeAssociation(foundCards);
       }
     }
     return foundAssociations;
 
-    function maybeAddOrientedAssociation(foundCards, value, orientation) {
+    function maybeAddOrientedAssociation(foundCards: Card[]) {
       if (foundCards.length >= 2) {
+        const value = foundCards[0].getValue();
+        const orientation = foundCards[0].getOrientation();
         foundAssociations.push(
           OrientedAssociationGateway.get(foundCards.length, value, orientation)
         );
--- src/business/data/oriented-associations.ts
+++ src/business/data/oriented-associations.ts
@@ -1,44 +1,32 @@
 export const OrientedAssociations = {
   '2': {
-    Seven: {
-      Up: 'Nouvelles',
-      Down: 'Femme de mauvaise vie'
-    },
-    Jack: {
-      Up: 'Inquiètude',
-      Down: 'Visite'
-    },
-    Queen: {
-      Up: 'Bonne amitié',
-      Down: 'Travail'
-    },
-    King: {
-      Up: 'Consultation',
-      Down: 'Projet'
-    }
+    Seven: { Up: 'Nouvelles', Down: 'Femme de mauvaise vie' },
+    Eight: { Up: '', Down: '' },
+    Nine: { Up: '', Down: '' },
+    Ten: { Up: '', Down: '' },
+    Jack: { Up: 'Inquiètude', Down: 'Visite' },
+    Queen: { Up: 'Bonne amitié', Down: 'Travail' },
+    King: { Up: 'Consultation', Down: 'Projet' },
+    Ace: { Up: '', Down: '' }
   },
   '3': {
-    Seven: {
-      Up: 'Contrariété',
-      Down: 'Allégresse'
-    },
-    Nine: {
-      Up: 'Succès moyen',
-      Down: 'Imprudence'
-    },
-    Ten: {
-      Up: 'Changement de situation',
-      Down: 'Déception'
-    }
+    Seven: { Up: 'Contrariété', Down: 'Allégresse' },
+    Eight: { Up: '', Down: '' },
+    Nine: { Up: 'Succès moyen', Down: 'Imprudence' },
+    Ten: { Up: 'Changement de situation', Down: 'Déception' },
+    Jack: { Up: '', Down: '' },
+    Queen: { Up: '', Down: '' },
+    King: { Up: '', Down: '' },
+    Ace: { Up: '', Down: '' }
   },
   '4': {
-    Seven: {
-      Up: 'Intrigues ténébreuses',
-      Down: 'Bataille'
-    },
-    Eight: {
-      Up: 'Ruine',
-      Down: 'Tromperie'
-    }
+    Seven: { Up: 'Intrigues ténébreuses', Down: 'Bataille' },
+    Eight: { Up: 'Ruine', Down: 'Tromperie' },
+    Nine: { Up: '', Down: '' },
+    Ten: { Up: '', Down: '' },
+    Jack: { Up: '', Down: '' },
+    Queen: { Up: '', Down: '' },
+    King: { Up: '', Down: '' },
+    Ace: { Up: '', Down: '' }
   }
 };
